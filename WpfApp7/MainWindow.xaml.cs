afe0507e2b23c6eb099a432be3c53d6ac04c4e00
Patrick Kranz <p.kranz@seeburger.de>
2021-02-15 16:04:35 +0100

    Add project files.

---
+++ WpfApp7/MainWindow.xaml.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            var vm = new Viewmodel();
+
+            vm.Data.Add(new SubViewmodel() { DisplayText = "Hallo" });
+            vm.Data.Add(new SubViewmodel() { DisplayText = "Welt" });
+            this.DataContext = vm;
+
+            this.Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // helps to set breakpoint in EditBox
+            //var box = VisualTreeHelpers.FindChild<InplaceEditBoxLib.Views.EditBox>(this);
+            //box.OnApplyTemplate();
+        }
+
+    }
+
+    public class Viewmodel
+    {
+        public ObservableCollection<SubViewmodel> Data { get; } = new ObservableCollection<SubViewmodel>();
+    }
+
+    public class SubViewmodel : DependencyObject
+    {
+
+        public ICommand RenameCommand { get; }
+
+        public SubViewmodel()
+        {
+            RenameCommand = new RenameCommand(this);
+        }
+
+        public string DisplayText
+        {
+            get { return (string)GetValue(DisplayTextProperty); }
+            set { SetValue(DisplayTextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for DisplayText.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty DisplayTextProperty =
+            DependencyProperty.Register("DisplayText", typeof(string), typeof(SubViewmodel), new PropertyMetadata(""));
+
+
+    }
+
+    internal class RenameCommand : ICommand
+    {
+        private SubViewmodel subViewmodel;
+
+        public RenameCommand(SubViewmodel subViewmodel)
+        {
+            this.subViewmodel = subViewmodel;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            var (newText, sender) = parameter as Tuple<string, object>;
+            this.subViewmodel.DisplayText = newText;
+        }
+    }
+
+    public class VisualTreeHelpers
+    {
+        /// <summary>
+        /// Returns the first ancester of specified type
+        /// </summary>
+        public static T FindAncestor<T>(DependencyObject current)
+        where T : DependencyObject
+        {
+            current = VisualTreeHelper.GetParent(current);
+
+            while (current != null)
+            {
+                if (current is T)
+                {
+                    return (T)current;
+                }
+                current = VisualTreeHelper.GetParent(current);
+            };
+            return null;
+        }
+
+        /// <summary>
+        /// Returns a specific ancester of an object
+        /// </summary>
+        public static T FindAncestor<T>(DependencyObject current, T lookupItem)
+        where T : DependencyObject
+        {
+            while (current != null)
+            {
+                if (current is T && current == lookupItem)
+                {
+                    return (T)current;
+                }
+                current = VisualTreeHelper.GetParent(current);
+            };
+            return null;
+        }
+
+        /// <summary>
+        /// Finds an ancestor object by name and type
+        /// </summary>
+        public static T FindAncestor<T>(DependencyObject current, string parentName)
+        where T : DependencyObject
+        {
+            while (current != null)
+            {
+                if (!string.IsNullOrEmpty(parentName))
+                {
+                    var frameworkElement = current as FrameworkElement;
+                    if (current is T && frameworkElement != null && frameworkElement.Name == parentName)
+                    {
+                        return (T)current;
+                    }
+                }
+                else if (current is T)
+                {
+                    return (T)current;
+                }
+                current = VisualTreeHelper.GetParent(current);
+            };
+
+            return null;
+
+        }
+
+        /// <summary>
+        /// Looks for a child control within a parent by name
+        /// </summary>
+        public static T FindChild<T>(DependencyObject parent, string childName)
+        where T : DependencyObject
+        {
+            // Confirm parent and childName are valid.
+            if (parent == null) return null;
+
+            T foundChild = null;
+
+            int childrenCount = VisualTreeHelper.GetChildrenCount(parent);
+            for (int i = 0; i < childrenCount; i++)
+            {
+                var child = VisualTreeHelper.GetChild(parent, i);
+                // If the child is not of the request child type child
+                T childType = child as T;
+                if (childType == null)
+                {
+                    // recursively drill down the tree
+                    foundChild = FindChild<T>(child, childName);
+
+                    // If the child is found, break so we do not overwrite the found child.
+                    if (foundChild != null) break;
+                }
+                else if (!string.IsNullOrEmpty(childName))
+                {
+                    var frameworkElement = child as FrameworkElement;
+                    // If the child's name is set for search
+                    if (frameworkElement != null && frameworkElement.Name == childName)
+                    {
+                        // if the child's name is of the request name
+                        foundChild = (T)child;
+                        break;
+                    }
+                    else
+                    {
+                        // recursively drill down the tree
+                        foundChild = FindChild<T>(child, childName);
+
+                        // If the child is found, break so we do not overwrite the found child.
+                        if (foundChild != null) break;
+                    }
+                }
+                else
+                {
+                    // child element found.
+                    foundChild = (T)child;
+                    break;
+                }
+            }
+
+            return foundChild;
+        }
+
+        /// <summary>
+        /// Looks for a child control within a parent by type
+        /// </summary>
+        public static T FindChild<T>(DependencyObject parent)
+            where T : DependencyObject
+        {
+            // Confirm parent is valid.
+            if (parent == null) return null;
+
+            T foundChild = null;
+
+            int childrenCount = VisualTreeHelper.GetChildrenCount(parent);
+            for (int i = 0; i < childrenCount; i++)
+            {
+                var child = VisualTreeHelper.GetChild(parent, i);
+                // If the child is not of the request child type child
+                T childType = child as T;
+                if (childType == null)
+                {
+                    // recursively drill down the tree
+                    foundChild = FindChild<T>(child);
+
+                    // If the child is found, break so we do not overwrite the found child.
+                    if (foundChild != null) break;
+                }
+                else
+                {
+                    // child element found.
+                    foundChild = (T)child;
+                    break;
+                }
+            }
+            return foundChild;
+        }
+    }
+}
